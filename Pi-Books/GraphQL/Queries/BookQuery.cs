eb4d30ef820acc6dbf3a5f3c895dcb3974545a4b
MD YASIR ARAFAT <icsm.arafat@gmail.com>
2022-06-03 00:09:29 +0600

    Added GraphQL Query to Get All The Books

---
+++ Pi-Books/GraphQL/Queries/BookQuery.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using Pi_Books.Data.Services;
+using Pi_Books.GraphQL.Types;
+
+namespace Pi_Books.GraphQL.Queries
+{
+    public class BookQuery : ObjectGraphType
+    {
+        public BookQuery(BooksService booksServices)
+        {
+            Field<ListGraphType<BookType>>(
+                "books",
+                "Returns list of books",
+                resolve: context => booksServices.GetAllBooks()
+                );
+        }
+    }
+}
