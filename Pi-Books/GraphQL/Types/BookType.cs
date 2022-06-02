160b5e281e4e1236277fb13497162db748fb906d
MD YASIR ARAFAT <icsm.arafat@gmail.com>
2022-06-03 00:01:18 +0600

    Added GraphQL Custom Object Type referencing Book Object Model in the API

---
+++ Pi-Books/GraphQL/Types/BookType.cs
@@ -0,0 +1,29 @@
+﻿using GraphQL.Types;
+using Pi_Books.Data.Models;
+
+namespace Pi_Books.GraphQL.Types
+{
+    public class BookType : ObjectGraphType<Book>
+    {
+        public BookType()
+        {
+            Field(x => x.Id, type: typeof(IdGraphType))
+                .Description("Id Property from the Book Model/Object.");
+
+            Field(x => x.Title, type: typeof(StringGraphType))
+                .Description("Title Property from the Book Model/Object.");
+
+            Field(x => x.Description, type: typeof(StringGraphType))
+                .Description("Description Property from the Book Model/Object.");
+
+            Field(x => x.Rating, type: typeof(IntGraphType))
+                .Description("Rating Property from the Book Model/Object.");
+
+            Field(x => x.DateAdded, type: typeof(DateTimeGraphType))
+                .Description("Date Added Property from the Book Model/Object.");
+
+            Field(x => x.DateRead, type: typeof(DateTimeGraphType))
+                .Description("Date Read Property from the Book Model/Object.");
+        }
+    }
+}
