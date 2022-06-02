7ba7d7e1079c232d9a7db517f2b65b1986b1e527
MD YASIR ARAFAT <icsm.arafat@gmail.com>
2022-06-03 00:26:12 +0600

    Added New Query Field

--- Pi-Books/GraphQL/Queries/BookQuery.cs
+++ Pi-Books/GraphQL/Queries/BookQuery.cs
@@ -1,4 +1,5 @@
-﻿using GraphQL.Types;
+﻿using GraphQL;
+using GraphQL.Types;
 using Pi_Books.Data.Services;
 using Pi_Books.GraphQL.Types;
 
@@ -13,6 +14,12 @@ namespace Pi_Books.GraphQL.Queries
                 "Returns list of books",
                 resolve: context => booksServices.GetAllBooks()
                 );
+
+            Field<BookType>(
+                    "book",
+                    "Returns an specific book by id.",
+                    new QueryArguments(new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "id", Description = "Book Id" }),
+                resolve: context => booksServices.GetTheBook(context.GetArgument("id", int.MinValue)));
         }
     }
 }
